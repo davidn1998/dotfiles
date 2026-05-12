51e3664998b4713a9db9dfc049fcf8ac5bd1d6b2
David Nwachukwu <davidnw1998@gmail.com>
2026-05-12 14:17:48 +0100

    Refactor lsp.lua to separate plugin specs properly

--- nvim/.config/nvim/lua/plugins/lsp/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp.lua
@@ -2,21 +2,23 @@ return {
   {
     "hrsh7th/cmp-nvim-lsp",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      { "antosha417/nvim-lsp-file-operations", config = true },
-      { "folke/lazydev.nvim", opts = {} },
-    },
     config = function()
-      -- import cmp-nvim-lsp plugin
-      local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-      -- used to enable autocompletion (assign to every lsp server config)
-      local capabilities = cmp_nvim_lsp.default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       vim.lsp.config("*", {
         capabilities = capabilities,
       })
     end,
+  },
+  {
+    "folke/lazydev.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {},
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    event = { "BufReadPre", "BufNewFile" },
+    config = true,
   },
   {
     "neovim/nvim-lspconfig",
