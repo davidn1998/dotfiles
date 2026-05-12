be356584fb17db3ca0d6a4669b16e83abc27abe3
David Nwachukwu <davidnw1998@gmail.com>
2026-05-12 19:53:59 +0100

    Make Neovim transparent to show through terminal background

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -4,6 +4,13 @@ return {
     priority = 1000,
     config = function()
       vim.cmd.colorscheme("gruvbox")
+
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+          vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        end,
+      })
     end,
   },
 }
