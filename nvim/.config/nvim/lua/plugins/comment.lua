3c2e919dafe43dffa03da73e8354b0f7002a8307
David Nwachukwu <davidnw1998@gmail.com>
2026-05-13 07:09:57 +0100

    Add inline plugin descriptions, suppress comment.nvim LSP warning, and disable lazy change notifications

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -10,9 +10,8 @@ return {
 
 		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
-		-- enable comment
+		---@diagnostic disable: missing-fields
 		comment.setup({
-			-- for commenting tsx, jsx, svelte, html files
 			pre_hook = ts_context_commentstring.create_pre_hook(),
 		})
 	end,
